a22cbf74ff08f8b6bec079b0f9109bb224103856
Nicholas Castro <10019281@sbschools.org>
2016-04-12 10:46:32 -0400

    started animating welcome screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-$document.ready(functon(){
+$(document).ready(function() {
 
+	//set playerName variable
+	var playerName = "";
 
-})
+	//hide player name
+	$('#player-name').hide();
+
+	//put starting cursor on name input
+	$("#name-input").focus();
+
+	//set function remove welcome screen
+	function removeWelcomeScreen(){
+		$("#welcome-screen").detach();
+	}
+
+	//on submit button click
+	$("#submit-button").on("click", function(e) {
+		e.preventDefault();
+		playerName = $("#name-input").val();
+		console.log(playerName);
+		$("#player-name").text(playerName);
+		$("#welcome-screen").addClass("animated hinge")
+		setTimeout(removeWelcomeScreen, 1000)
+		$("#player-name").show().addClass("animated fadeIn");
+	});
+});
